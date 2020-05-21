be8e309b2ffca1e34ce843b74b7ef03afcd3e580
shivankbhardwaj <55373727+shivankbhardwaj@users.noreply.github.com>
2020-05-21 23:38:15 +0530

    Add files via upload

---
+++ date.java
@@ -0,0 +1,12 @@
+import java.util.Date;
+public class DateDemo {
+
+   public static void main(String args[]) {
+      
+      Date date = new Date();
+
+      String str = String.format("Current Date/Time : %tc", date );
+
+      System.out.printf(str);
+   }
+}
